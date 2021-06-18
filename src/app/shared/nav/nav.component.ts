1070321c76e3ea6f839b85be78f490bc57ffd2fe
waygon32 <truonthien2310@gmail.com>
2021-06-18 16:40:37 +0700

    1 s truoc tham hoa

--- src/app/shared/nav/nav.component.ts
+++ src/app/shared/nav/nav.component.ts
@@ -27,7 +27,7 @@ export class NavComponent implements OnInit {
   }
 
   logout() {
-    this.authenticationService.logout().subscribe();
+    this.authenticationService.logout();
     this.router.navigate(['/house']);
   }
 
