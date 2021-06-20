2ba5c424f2f72259f5d4a1cdb863d9d599c9f0c2
tiep.nguyen <mrkimhp942@gmail.com>
2021-06-20 18:54:27 +0700

    Add

---
+++ src/app/service/review/review.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {environment} from '../../../environments/environment';
+import {Observable} from 'rxjs';
+
+import {Review} from '../../model/review';
+
+
+const API_URL = `${environment.api_url}`;
+@Injectable({
+  providedIn: 'root'
+})
+export class ReviewService {
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getAllReview(houseId: number): Observable<Review[]> {
+    return this.http.get<Review[]>(API_URL + `/houses/reviews/${houseId}`);
+  }
+  createReview(houseId: number,review:Review): Observable<Review> {
+    return this.http.post<Review>(API_URL + `/houses/createReview/${houseId}`,review);
+  }
+}
